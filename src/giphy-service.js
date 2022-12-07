e3d2debbda71958c4607a27cf6112fd21872d5ee
Emzzy241 <emzzyoluwole@gmail.com>
2022-12-07 16:12:03 +0100

    adding my business logic file

--- src/giphy-service.js
+++ src/giphy-service.js
@@ -9,32 +9,32 @@ export class SearchGiphyService {
     // the static method is just a method that returns a promise object, and we are returning a promise object because we did not store the promise in a variable just like we did before
     static getSearchedGif(userGif) {
         // using the return keyword for returning our promise so our function is not undefined
-        return new Promise(function (gifPromiseResolved, gifPromiseRejected) {
-            // reinstantiating an API request
-            let giphySearchRequest = new XMLHttpRequest();
-
-            // getting giphy's search endpoint url and passing user's value in it
-
-            const giphySearchUrl = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userGif}&limit=1&offset=0&rating=g&lang=en`
 
 
-            giphySearchRequest.onload = function () {
-                if (this.status === 200) {
-                    // we used gifPromiseResolved or gifPromiseRejected to determine whether a promise should be resolved or rejected
-                    gifPromiseResolved(giphySearchRequest.response);
+            // using the return keyword to fetch giphy's search endpoint url and passing user's value in it
+
+            return fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userGif}&limit=1&offset=0&rating=g&lang=en`)
+            // no semicolons in fetch because of the .then()
+
+            // since a fetch also returns a promise, we can use .then() on it 
+            // the .then() will be used here will determine if I get a resolved promise(which should return me the data) or a rejected Promise(In this case I throw in an Error)
+
+            .then( function(giphySearchResponse){
+                // checking to see if giphyResponse has an ok property, if it does, then I have a successful response... If it doesn't I throw in an error that would be handled in the cath block
+                if(!giphySearchResponse.ok){
+                    // displaying the statusText property when handling my Errors
+                    throw Error(giphySearchResponse.statusText);
                 }
-                else {
-                    gifPromiseRejected(giphySearchRequest.response)
-                }
+                    // calling the .json() method on my giphyResponse if I do get a successful call
+                    giphySearchResponse.json();
+            })
 
-            }
+            // the catch block is where I handle the error I threw above in the if statement
+            .catch( function(giphyError){
+                // returning the giphyError parameter itself when I get an error
+                return giphyError;
+            })
 
-            // opening and sending a request to the gif server
-
-            giphySearchRequest.open("GET", giphySearchUrl, true);
-            giphySearchRequest.send();
-
-        });
 
 
     }
@@ -49,39 +49,26 @@ export class RandomGif {
     // the static method is just a method that returns a promise object, and we are returning a promise object because we did not store the promise in a variable just like we did before
 
     static getRandomGif() {
-        // using the return keyword for returning our promise so our function is not undefined
-        return new Promise(function (randomGifResolved, randomGifRejected) {
-            // reinstantiating another API request
-            let giphyRandomRequest = new XMLHttpRequest();
 
-            // getting giphy's random endpoint(an endpoint is just like a url used for api calls)
-            const giphyRandomUrl = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g`
+            // fetching giphy's random endpoint(an endpoint is just like a url used for api calls)
+            return fetch(`https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g`)
 
-            // using an .onload property here instead of the .onreadystatechange is really great as we no longer have to specify a readyState of 4, 
-            // and also it will be triggered once: (when the response has loaded) and not everytime the readyState has changes
+            //handling the Promise fetch would be returning for me with a .then()
 
-            // to summarize: using an onload propery here means we are only waiting for the request to load only
-            giphyRandomRequest.onload = function () {
-
-                // running a branch to give me whas in the response property whether the Status = 200 or not
-
-                if (this.status === 200) {
-                    // we used promiseResolved or promiseRejected to determine whether a promise should be resolved or rejected
-                    randomGifResolved(giphyRandomRequest.response);
+            // a .then() that helps me to determine both a successful and an unsuccessful call
+            // don't forget; a .then() method takes in an anonymous function
+            .then( (giphyRandomResponse)=>{
+                // using a branch to determine a resolved or a rejected promise
+                if(!giphyRandomResponse.ok){
+                    throw Error(giphyRandomResponse.statusText);
                 }
-                else {
-                    randomGifRejected(giphyRandomRequest.response)
-                }
+                return giphyRandomResponse.json();
+            })
 
-            }
-
-            // opening and sending a request to the gif server
-
-            giphyRandomRequest.open("GET", giphyRandomUrl, true)
-            giphyRandomRequest.send();
-
-
-        });
+            // a catch block for handling the error thrown above
+            .catch( function(giphyRandomError){
+                return giphyRandomError;
+            })
     }
 }
 
@@ -91,37 +78,25 @@ export class RandomGif {
 export class TrendingGif {
 
     // a static method for running the trending gif API call
-    // the static method is just a method that returns a promise object, and we are returning a promise object because we did not store the promise in a variable just like we did before
-
+    
     static getTrendingGif() {
-        // using the return keyword for returning our promise so our function is not undefined
-        return new Promise(function (trendingGifResolved, trendingGiffRejected) {
 
-            // reinstantiating an API request for trending gifs --- This will ensure the request is empty and not give me a request I have previously used
-            let giphyTrendingRequest = new XMLHttpRequest();
 
-            // getting a giphy trend endpoint url for users
 
-            const giphyTrendUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&tag=&rating=g`
+            // fetching a giphy trend endpoint url for users
 
-            // using the onload property to wait for the request to load
+            return fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&tag=&rating=g`)
 
-            giphyTrendingRequest.onload = function () {
-                // running another branch to determine whether the status of the trend promise is = 200
-                if (this.status === 200) {
-                    trendingGifResolved(giphyTrendingRequest.response);
+            .then( (giphyTrendResponse)=>{
+                if(!giphyTrendResponse.ok){
+                    throw Error(giphyTrendResponse);
                 }
-                else {
-                    trendingGiffRejected(giphyTrendingRequest.response);
-                }
-            }
+                return giphyTrendResponse.json();
+            })
 
-
-            // time to open and send the request 
-
-            giphyTrendingRequest.open("GET", giphyTrendUrl, true);
-            giphyTrendingRequest.send();
-
-        });
+            // the .catch block for handling the errors thrown above
+            .catch((giphyTrendError)=>{
+                return giphyTrendError;
+            })
     }
 }
